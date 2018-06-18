9d53e720063ad30761e4ab864e02baf2a8584f87
Marlon Pluas <mpluasgu@hotmail.com>
2018-06-18 00:44:13 -0500

    Tipo de datos

---
+++ CursoSE/src/tipodedatos/datos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatos;
+
+/**
+ *
+ * @author mpluas
+ */
+public class datos {
+    
+    public static void main(String[] args) {
+        int var1; //Dato primitivo || Solo aceptan valores numericos 1,2,3,4,5.... n
+        double vard;
+        Integer var2; //Dato no primitivo || Aceptan valores o condiciones que se asocian a una clase ejmp null
+        String cadena;
+        var1 = 1;
+        vard = 2.5;
+        var2 = null;
+        cadena = "marlon";
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(vard);
+        System.out.println(cadena.toUpperCase());
+    }
+
+}
